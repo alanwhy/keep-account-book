e021078a542ab567891d88af94fd265a9265d212
吴浩原 <570028237@qq.com>
2020-05-14 22:27:36 +0800

    价格条码完成一版

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import PriceList from "./components/PriceList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    id: 1,
+    title: "去云南旅游",
+    price: 200,
+    date: "2018-09-10",
+    category: {
+      id: 1,
+      name: "旅行",
+      type: "outcome",
+    },
+  },
+  {
+    id: 2,
+    title: "去云南旅游",
+    price: 300,
+    date: "2018-09-10",
+    category: {
+      id: 1,
+      name: "旅行",
+      type: "outcome",
+    },
+  },
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <PriceList
+          items={items}
+          onModifyItem={(item) => {
+            console.log(item.id);
+          }}
+          onDeleteItem={(item) => {
+            console.log(item.id);
+          }}
+        ></PriceList>
+      </div>
+    );
+  }
 }
 
 export default App;
