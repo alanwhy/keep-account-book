cce2fff17f97fe0c5156ac1b01c721eb653c4a0c
吴浩原 <570028237@qq.com>
2020-05-15 13:22:43 +0800

    选择时间初始化

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+// import "./App.css";
 import PriceList from "./components/PriceList";
 import ViewTab from "./components/ViewTab";
 import { LIST_VIEW } from "./utility";
+import MonthPicker from "./components/MonthPicker";
 
 const items = [
   {
@@ -37,6 +38,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <MonthPicker year={2018} month={5}></MonthPicker>
         <ViewTab
           activeTab={LIST_VIEW}
           onTabChange={(view) => {
