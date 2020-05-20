b5778be58cc965f59b2f6935fa9962d9ea9716f4
wuhaoyuan <wuhaoyuan@cennavi.com.cn>
2020-05-20 13:25:07 +0800

    完成静态页面的交互

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PriceList from "./../components/PriceList";
 import ViewTab from "./../components/ViewTab";
-import { LIST_VIEW, TYPE_OUTCOME, parseToYearAndMonth } from "./../utility";
+import {
+  LIST_VIEW,
+  TYPE_OUTCOME,
+  parseToYearAndMonth,
+  CHART_VIEW,
+  padLeft,
+} from "./../utility";
 import MonthPicker from "./../components/MonthPicker";
 import CreateBtn from "./../components/CreateBtn";
 import TotalPrice from "./../components/TotalPrice";
@@ -19,8 +25,8 @@ const categorys = {
     name: "理财",
     type: "income",
     iconName: "logo-yen",
-  }
-}
+  },
+};
 
 const items = [
   {
@@ -28,32 +34,82 @@ const items = [
     title: "去云南旅游",
     price: 200,
     date: "2018-09-10",
-    cid: "1"
+    cid: "1",
   },
   {
     id: 2,
     title: "炒股",
     price: 300,
     date: "2018-09-10",
-    cid: "2"
+    cid: "2",
   },
 ];
 
+const newItem = {
+  id: 4,
+  title: "新添加的项目",
+  price: 300,
+  date: "2018-10-10",
+  cid: "1",
+};
+
 class Home extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       items,
       currentDate: parseToYearAndMonth(),
       tabView: LIST_VIEW,
-    }
+    };
   }
+  changeView = (view) => {
+    this.setState({
+      tabView: view,
+    });
+  };
+  changeDate = (year, month) => {
+    this.setState({
+      currentDate: { year, month },
+    });
+  };
+  modifyItem = (modifiedItem) => {
+    const modifiedItems = this.state.items.map((item) => {
+      if (item.id === modifiedItem.id) {
+        return { ...item, title: "更新后的标题" };
+      } else {
+        return item;
+      }
+    });
+    this.setState({
+      items: modifiedItems,
+    });
+  };
+  createItem = () => {
+    this.setState({
+      items: [newItem, ...this.state.items],
+    });
+  };
+  deleteItem = (deletedItem) => {
+    const filteredItems = this.state.items.filter((item) => {
+      return item.id !== deletedItem.id;
+    });
+    this.setState({
+      items: filteredItems,
+    });
+  };
   render() {
-    const { items, currentDate, tabView } = this.state
-    const itemsWithCategory = items.map(item => {
-      item.category = categorys[item.id]
-      return item
-    })
+    const { items, currentDate, tabView } = this.state;
+    const itemsWithCategory = items
+      .map((item) => {
+        item.category = categorys[item.cid];
+        return item;
+      })
+      .filter((item) => {
+        return item.date.includes(
+          `${currentDate.year}-${padLeft(currentDate.month)}`
+        );
+      });
+    console.log(itemsWithCategory);
     let totalIncome = 0;
     let totalOutcome = 0;
     itemsWithCategory.forEach((item) => {
@@ -72,7 +128,7 @@ class Home extends React.Component {
               <MonthPicker
                 year={currentDate.year}
                 month={currentDate.month}
-                onChange={() => { }}
+                onChange={this.changeDate}
               ></MonthPicker>
             </div>
             <div className="col">
@@ -84,13 +140,16 @@ class Home extends React.Component {
           </div>
         </header>
         <div className="content-area py-3 px-3">
-          <ViewTab activeTab={tabView} onTabChange={() => { }}></ViewTab>
-          <CreateBtn onClick={() => { }}></CreateBtn>
-          <PriceList
-            items={items}
-            onModifyItem={() => { }}
-            onDeleteItem={() => { }}
-          ></PriceList>
+          <ViewTab activeTab={tabView} onTabChange={this.changeView}></ViewTab>
+          <CreateBtn onClick={this.createItem}></CreateBtn>
+          {tabView === LIST_VIEW && (
+            <PriceList
+              items={itemsWithCategory}
+              onModifyItem={this.modifyItem}
+              onDeleteItem={this.deleteItem}
+            ></PriceList>
+          )}
+          {tabView === CHART_VIEW && <h1>这里是图表</h1>}
         </div>
       </React.Fragment>
     );
