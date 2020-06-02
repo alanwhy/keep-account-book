c8669f90854ee9b1d04d7192430e9da4c89e693e
wuhaoyuan <wuhaoyuan@cennavi.com.cn>
2020-06-02 14:59:35 +0800

    完成所有组件逻辑，还没有mock数据

--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -1,59 +1,64 @@
 import React from "react";
 import Ionicon from "react-ionicons";
 import PropTypes from "prop-types";
-
+import { Colors } from "../utility";
 const PriceList = ({ items, onModifyItem, onDeleteItem }) => {
   return (
     <ul className="list-group list-group-flush">
       {items.map((item) => (
         <li
-          className="list-group-item d-flex justify-content-between align-items-center"
+          className="list-group-item d-flex 
+            justify-content-between align-items-center"
           key={item.id}
         >
           <span className="col-1">
             <Ionicon
               className="rounded-circle"
               fontSize="30px"
-              style={{ backgroundColor: "#0076ff", padding: "5px" }}
+              style={{ backgroundColor: Colors.blue, padding: "5px" }}
               color={"#fff"}
               icon={item.category.iconName}
-            ></Ionicon>
+            />
           </span>
           <span className="col-5">{item.title}</span>
           <span className="col-2 font-weight-bold">
-            {item.category.type === "income" ? "" : "-"}
+            {item.category.type === "income" ? "+" : "-"}
             {item.price}元
           </span>
           <span className="col-2">{item.date}</span>
           <a
-            href="/#"
             className="col-1"
-            onClick={() => {
+            href="/"
+            role="button"
+            onClick={(event) => {
+              event.preventDefault();
               onModifyItem(item);
             }}
           >
             <Ionicon
               className="rounded-circle"
               fontSize="30px"
-              style={{ backgroundColor: "#28a745", padding: "5px" }}
+              style={{ backgroundColor: Colors.green, padding: "5px" }}
               color={"#fff"}
               icon="ios-create-outline"
-            ></Ionicon>
+            />
           </a>
           <a
-            href="/#"
             className="col-1"
-            onClick={() => {
+            href="/"
+            role="button"
+            onClick={(event) => {
+              event.preventDefault();
               onDeleteItem(item);
             }}
           >
             <Ionicon
               className="rounded-circle"
               fontSize="30px"
-              style={{ backgroundColor: "#dc3545", padding: "5px" }}
+              style={{ backgroundColor: Colors.red, padding: "5px" }}
               color={"#fff"}
               icon="ios-close"
-            ></Ionicon>
+            />
           </a>
         </li>
       ))}
@@ -66,9 +71,5 @@ PriceList.propTypes = {
   onModifyItem: PropTypes.func.isRequired,
   onDeleteItem: PropTypes.func.isRequired,
 };
-
-// PriceList.defaultProps = {
-//   onModifyItem: () => {},
-// };
 
 export default PriceList;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,159 +1,162 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import PriceList from "./../components/PriceList";
-import ViewTab from "./../components/ViewTab";
-import {
-  LIST_VIEW,
-  TYPE_OUTCOME,
-  parseToYearAndMonth,
-  CHART_VIEW,
-  padLeft,
-} from "./../utility";
-import MonthPicker from "./../components/MonthPicker";
-import CreateBtn from "./../components/CreateBtn";
-import TotalPrice from "./../components/TotalPrice";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Ionicon from 'react-ionicons'
+import logo from '../logo.svg'
+import { withRouter } from 'react-router-dom'
+import PieChart from '../components/PieChart'
+import { LIST_VIEW, CHART_VIEW, TYPE_INCOME, TYPE_OUTCOME, Colors} from '../utility'
+import PriceList from '../components/PriceList'
+import MonthPicker from '../components/MonthPicker'
+import CreateBtn from '../components/CreateBtn'
+import TotalPrice from '../components/TotalPrice'
+import Loader from '../components/Loader'
+import { Tabs, Tab } from '../components/Tabs'
+import withContext from '../WithContext'
 
-export const categorys = {
-  "1": {
-    id: 1,
-    name: "旅行",
-    type: "outcome",
-    iconName: "ios-plane",
-  },
-  "2": {
-    id: 2,
-    name: "理财",
-    type: "income",
-    iconName: "logo-yen",
-  },
-};
+const tabsText = [LIST_VIEW, CHART_VIEW]
 
-export const items = [
-  {
-    id: 1,
-    title: "去云南旅游",
-    price: 200,
-    date: "2020-05-10",
-    cid: "1",
-  },
-  {
-    id: 2,
-    title: "炒股",
-    price: 300,
-    date: "2020-05-10",
-    cid: "2",
-  },
-];
+const generateChartDataByCategory = (items, type = TYPE_OUTCOME) => {
+  let categoryMap = { }
+  items.filter(item => item.category.type === type).forEach((item) => {
+    if (categoryMap[item.cid]) {
+      categoryMap[item.cid].value += (item.price * 1)
+      categoryMap[item.cid].items = [...categoryMap[item.cid].items, item.id]
+    } else {
+      categoryMap[item.cid] = {
+        category: item.category,
+        value: item.price * 1,
+        items: [item.id]
+      }
+    }
+  })
+  return Object.keys(categoryMap).map(mapKey => ({ ...categoryMap[mapKey], name: categoryMap[mapKey].category.name }))
+}
 
-const newItem = {
-  id: 4,
-  title: "新添加的项目",
-  price: 300,
-  date: "2020-04-10",
-  cid: "1",
-};
-
-class Home extends React.Component {
+export class Home extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      items,
-      currentDate: parseToYearAndMonth(),
-      tabView: LIST_VIEW,
-    };
+      tabView: tabsText[0],
+    }
   }
-  changeView = (view) => {
+  componentDidMount() {
+    this.props.actions.getInitalData()
+  }
+  changeView = (index) => {
     this.setState({
-      tabView: view,
-    });
-  };
+      tabView: tabsText[index],
+    })
+  }
   changeDate = (year, month) => {
-    this.setState({
-      currentDate: { year, month },
-    });
-  };
-  modifyItem = (modifiedItem) => {
-    const modifiedItems = this.state.items.map((item) => {
-      if (item.id === modifiedItem.id) {
-        return { ...item, title: "更新后的标题" };
-      } else {
-        return item;
-      }
-    });
-    this.setState({
-      items: modifiedItems,
-    });
-  };
+    this.props.actions.selectNewMonth(year, month)
+  }
   createItem = () => {
-    this.setState({
-      items: [newItem, ...this.state.items],
-    });
-  };
-  deleteItem = (deletedItem) => {
-    const filteredItems = this.state.items.filter((item) => {
-      return item.id !== deletedItem.id;
-    });
-    this.setState({
-      items: filteredItems,
-    });
-  };
+    this.props.history.push('/create')
+  }
+  modifyItem = (item) => {
+    this.props.history.push(`/edit/${item.id}`)
+  }
+  deleteItem = (item) => {
+    this.props.actions.deleteItem(item)
+  }
   render() {
-    const { items, currentDate, tabView } = this.state;
-    const itemsWithCategory = items
-      .map((item) => {
-        item.category = categorys[item.cid];
-        return item;
-      })
-      .filter((item) => {
-        return item.date.includes(
-          `${currentDate.year}-${padLeft(currentDate.month)}`
-        );
-      });
-    console.log(itemsWithCategory);
-    let totalIncome = 0;
-    let totalOutcome = 0;
-    itemsWithCategory.forEach((item) => {
+    const { data } = this.props
+    const { items, categories, currentDate, isLoading } = data
+    const { tabView } = this.state
+    const tabIndex = tabsText.findIndex(tabText => tabText === tabView)
+    const itemsWithCategory = Object.keys(items).map(id => {
+      items[id].category = categories[items[id].cid]
+      return items[id]
+    })
+    let totalIncome = 0, totalOutcome = 0
+    itemsWithCategory.forEach(item => {
       if (item.category.type === TYPE_OUTCOME) {
-        totalOutcome += item.price;
+        totalOutcome += item.price
       } else {
-        totalIncome += item.price;
+        totalIncome += item.price
       }
-    });
+    })
+    const chartOutcomDataByCategory = generateChartDataByCategory(itemsWithCategory, TYPE_OUTCOME)
+    const chartIncomeDataByCategory = generateChartDataByCategory(itemsWithCategory, TYPE_INCOME)
     return (
       <React.Fragment>
-        <header className="App-header">
-          <div className="row mb-5 ml-2">账本</div>
-          <div className="row">
-            <div className="col">
-              <MonthPicker
-                year={currentDate.year}
-                month={currentDate.month}
-                onChange={this.changeDate}
-              ></MonthPicker>
+        <div className="App-header">
+            <div className="row mb-5 justify-content-center">
+              <img src={logo} className="App-logo" alt="logo" />
             </div>
-            <div className="col">
-              <TotalPrice
-                income={totalIncome}
-                outcome={totalOutcome}
-              ></TotalPrice>
+            <div className="row">
+              <div className="col">
+                <MonthPicker
+                  year={currentDate.year}
+                  month={currentDate.month}
+                  onChange={this.changeDate}
+                />
+              </div>
+              <div className="col">
+                <TotalPrice
+                  income={totalIncome}
+                  outcome={totalOutcome}
+                />
+              </div>
             </div>
           </div>
-        </header>
-        <div className="content-area py-3 px-3">
-          <ViewTab activeTab={tabView} onTabChange={this.changeView}></ViewTab>
-          <CreateBtn onClick={this.createItem}></CreateBtn>
-          {tabView === LIST_VIEW && (
-            <PriceList
-              items={itemsWithCategory}
-              onModifyItem={this.modifyItem}
-              onDeleteItem={this.deleteItem}
-            ></PriceList>
-          )}
-          {tabView === CHART_VIEW && <h1>这里是图表</h1>}
-        </div>
-      </React.Fragment>
-    );
+          <div className="content-area py-3 px-3">
+            { isLoading &&
+              <Loader />
+            }
+            { !isLoading &&
+            <React.Fragment>
+            <Tabs activeIndex={tabIndex} onTabChange={this.changeView}>
+              <Tab>
+                <Ionicon 
+                  className="rounded-circle mr-2" 
+                  fontSize="25px"
+                  color={Colors.blue}
+                  icon='ios-paper'
+                />
+                列表模式
+              </Tab>
+              <Tab>
+                <Ionicon 
+                  className="rounded-circle mr-2" 
+                  fontSize="25px"
+                  color={Colors.blue}
+                  icon='ios-pie'
+                />
+                图表模式
+              </Tab>
+            </Tabs>
+            <CreateBtn onClick={this.createItem} />
+            { tabView === LIST_VIEW && itemsWithCategory.length > 0 &&
+              <PriceList 
+                items={itemsWithCategory}
+                onModifyItem={this.modifyItem}
+                onDeleteItem={this.deleteItem}
+              />
+            }
+            { tabView === LIST_VIEW && itemsWithCategory.length === 0 &&
+              <div className="alert alert-light text-center no-record">
+                您还没有任何记账记录
+              </div>
+            }
+            { tabView === CHART_VIEW &&
+              <React.Fragment>
+                <PieChart title="本月支出" categoryData={chartOutcomDataByCategory} />
+                <PieChart title="本月收入" categoryData={chartIncomeDataByCategory} />
+              </React.Fragment>
+            }
+            </React.Fragment>
+          }
+          </div>
+        </React.Fragment>
+    )
   }
 }
 
-export default Home;
+Home.propTypes = {
+  data: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  history: PropTypes.object,
+}
+
+export default withRouter(withContext(Home))
